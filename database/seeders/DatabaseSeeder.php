d569f12dfd7a4cf515fe01086696841cdad732ec
Nathan Setiawan <nathanmabes@gmail.com>
2022-03-19 09:57:57 +0700

    Finish Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CampTableSeeder::class,
+            CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
+        ]);
     }
 }
