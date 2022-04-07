7f7345a2129b4deef874c7e418fff63097be790e
Nathan Setiawan <nathanmabes@gmail.com>
2022-04-07 18:44:45 +0700

    Finish Integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <button id="btn-modal" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="visibility: hidden"></button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Let's join us!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <b>Hi folks!</b> <br>
+                what are you waiting for? get your own class by hit the button below!
+            </div>
+            <div class="modal-footer">
+                <a href="http://joinbwa.com/laravel8" class="btn btn-primary">Let's Go</a>
+            </div>
+            </div>
+        </div>
+    </div>
     <section class="banner">
         <div class="container">
             <div class="row justify-content-center">
@@ -24,7 +42,7 @@
                         </div>
                         <div class="col-lg-6 col-12 text-center">
                             <a href="#">
-                                <img src="{{ asset('images/banner.png ') }}" class="img-fluid" alt="">
+                                <img src="{{asset('images/banner.png')}}" class="img-fluid" alt="">
                             </a>
                         </div>
                     </div>
@@ -32,7 +50,7 @@
             </div>
             <div class="row brands">
                 <div class="col-lg-12 col-12 text-center">
-                    <img src="{{ asset('images/brands.png ') }}" alt="">
+                    <img src="{{asset('images/brands.png')}}" alt="">
                 </div>
             </div>
         </div>
@@ -54,7 +72,7 @@
             <div class="row">
                 <div class="col-lg-3 col-12">
                     <div class="item-benefit">
-                        <img src="{{ asset('images/ic_globe.png ') }}" class="icon" alt="">
+                        <img src="{{asset('images/ic_globe.png')}}" class="icon" alt="">
                         <h3 class="title">
                             Diversity
                         </h3>
@@ -65,7 +83,7 @@
                 </div>
                 <div class="col-lg-3 col-12">
                     <div class="item-benefit">
-                        <img src="{{ asset('images/ic_globe-1.png ') }}" class="icon" alt="">
+                        <img src="{{asset('images/ic_globe-1.png')}}" class="icon" alt="">
                         <h3 class="title">
                             A.I Guideline
                         </h3>
@@ -76,7 +94,7 @@
                 </div>
                 <div class="col-lg-3 col-12">
                     <div class="item-benefit">
-                        <img src="{{ asset('images/ic_globe-2.png ') }}" class="icon" alt="">
+                        <img src="{{asset('images/ic_globe-2.png')}}" class="icon" alt="">
                         <h3 class="title">
                             1-1 Mentoring
                         </h3>
@@ -87,7 +105,7 @@
                 </div>
                 <div class="col-lg-3 col-12">
                     <div class="item-benefit">
-                        <img src="{{ asset('images/ic_globe-3.png ') }}" class="icon" alt="">
+                        <img src="{{asset('images/ic_globe-3.png')}}" class="icon" alt="">
                         <h3 class="title">
                             Future Job
                         </h3>
@@ -104,7 +122,7 @@
         <div class="container">
             <div class="row item-step pb-70">
                 <div class="col-lg-6 col-12 text-center">
-                    <img src="{{ asset('images/step1.png ') }}" class="cover" alt="">
+                    <img src="{{asset('images/step1.png')}}" class="cover" alt="">
                 </div>
                 <div class="col-lg-6 col-12 text-left copywriting">
                     <p class="story">
@@ -141,13 +159,13 @@
                     </p>
                 </div>
                 <div class="col-lg-6 col-12 text-center">
-                    <img src="{{ asset('images/step2.png ') }}" class="cover" alt="">
+                    <img src="{{asset('images/step2.png')}}" class="cover" alt="">
                 </div>
 
             </div>
             <div class="row item-step">
                 <div class="col-lg-6 col-12 text-center">
-                    <img src="{{ asset('images/step3.png ') }}" class="cover" alt="">
+                    <img src="{{asset('images/step3.png')}}" class="cover" alt="">
                 </div>
                 <div class="col-lg-6 col-12 text-left copywriting">
                     <p class="story">
@@ -199,7 +217,7 @@
                                     $280K
                                 </h1>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Pro Techstack Kit
                                     </p>
@@ -207,7 +225,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         iMac Pro 2021 & Display
                                     </p>
@@ -215,7 +233,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         1-1 Mentoring Program
                                     </p>
@@ -223,7 +241,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Final Project Certificate
                                     </p>
@@ -231,7 +249,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Offline Course Videos
                                     </p>
@@ -239,7 +257,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Future Job Opportinity
                                     </p>
@@ -247,7 +265,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Premium Design Kit
                                     </p>
@@ -255,14 +273,14 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Website Builder
                                     </p>
                                     <div class="clear"></div>
                                 </div>
                                 <p>
-                                    <a href="{{route('checkout')}}" class="btn btn-master btn-primary w-100 mt-3">
+                                    <a href="{{route('checkout.create', 'gila-belajar')}}" class="btn btn-master btn-primary w-100 mt-3">
                                         Take This Plan
                                     </a>
                                 </p>
@@ -277,7 +295,7 @@
                                     $140K
                                 </h1>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         1-1 Mentoring Program
                                     </p>
@@ -285,7 +303,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Final Project Certificate
                                     </p>
@@ -293,7 +311,7 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing mb-4">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Offline Course Videos
                                     </p>
@@ -301,14 +319,14 @@
                                     <div class="divider"></div>
                                 </div>
                                 <div class="item-benefit-pricing">
-                                    <img src="{{ asset('images/ic_check.svg') }}" alt="">
+                                    <img src="{{asset('images/ic_check.svg')}}" alt="">
                                     <p>
                                         Future Job Opportinity
                                     </p>
                                     <div class="clear"></div>
                                 </div>
                                 <p>
-                                    <a href="{{route('checkout')}}" class="btn btn-master btn-secondary w-100 mt-3">
+                                    <a href="{{route('checkout.create', 'baru-mulai')}}" class="btn btn-master btn-secondary w-100 mt-3">
                                         Start With This Plan
                                     </a>
                                 </p>
@@ -319,7 +337,7 @@
             </div>
             <div class="row pb-70">
                 <div class="col-lg-12 col-12 text-center">
-                    <img src="{{ asset('images/brands.png') }}" height="30" alt="">
+                    <img src="{{asset('images/brands.png')}}" height="30" alt="">
                 </div>
             </div>
         </div>
@@ -342,12 +360,12 @@
                     <div class="row">
                         <div class="col-lg-4 col-12">
                             <div class="item-review">
-                                <img src="{{ asset('images/stars.svg') }}" alt="">
+                                <img src="{{asset('images/stars.svg')}}" alt="">
                                 <p class="message">
                                     I was not really into code but after they teach me how to train my logic then I was really fall in love with code
                                 </p>
                                 <div class="user">
-                                    <img src="{{ asset('images/fanny_photo.png') }}" class="photo" alt="">
+                                    <img src="{{asset('images/fanny_photo.png')}}" class="photo" alt="">
                                     <div class="info">
                                         <h4 class="name">
                                             Fanny
@@ -361,12 +379,12 @@
                         </div>
                         <div class="col-lg-4 col-12">
                             <div class="item-review">
-                                <img src="{{ asset('images/stars.svg') }}" alt="">
+                                <img src="{{asset('images/stars.svg')}}" alt="">
                                 <p class="message">
                                     Code is really important if we want to build a company and strike to the win
                                 </p>
                                 <div class="user">
-                                    <img src="{{ asset('images/angga.png') }}" class="photo" alt="">
+                                    <img src="{{asset('images/angga.png')}}" class="photo" alt="">
                                     <div class="info">
                                         <h4 class="name">
                                             Angga
@@ -380,12 +398,12 @@
                         </div>
                         <div class="col-lg-4 col-12">
                             <div class="item-review">
-                                <img src="{{ asset('images/stars.svg') }}" alt="">
+                                <img src="{{asset('images/stars.svg')}}" alt="">
                                 <p class="message">
                                     My background is design and art but I do really love how to make my design working in the development phase
                                 </p>
                                 <div class="user">
-                                    <img src="{{ asset('images/beatrice.png') }}" class="photo" alt="">
+                                    <img src="{{asset('images/beatrice.png')}}" class="photo" alt="">
                                     <div class="info">
                                         <h4 class="name">
                                             Beatrice
