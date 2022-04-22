686a49c171b5ce8a057f9823f9ba0de612b2dd1c
Nathan Setiawan <nathanmabes@gmail.com>
2022-04-22 12:10:14 +0700

    Update Admin

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\DashboardController as UserDashboard;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\CheckoutController as AdminCheckout;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -34,12 +38,26 @@ Route::get('auth/google/callback', [UserController::class, 'handleProviderCallba
 Route::middleware(['auth'])->group(function () {
 
     //checkout routes
-    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
-    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
-    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success')->middleware('EnsureUserRole:user');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create')->middleware('EnsureUserRole:user');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store')->middleware('EnsureUserRole:user');
+
+    //dashboard
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
 
     //user dashboard
-    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+    Route::prefix('user/dashboard')->namespace('User')->name('user.')->middleware('EnsureUserRole:user')->group(function(){
+        Route::get('/', [UserDashboard::class, 'index'])->name('dashboard');
+    });
+
+    //admin dashboard
+    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('EnsureUserRole:admin')->group(function(){
+        Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+        
+        //admin checkout
+        Route::post('checkout/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
+    });
+
     
 });
 
